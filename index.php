09a66059e3c730c56dc848008186dc52391c9fe5
aliffadhil28 <alif70662@gmail.com>
2023-04-15 18:11:11 +0700

    first-commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <!-- <style>
+        <?php
+        include'css/index.css';
+    ?>
+    </style> -->
+    <title>Data Diri</title>
+</head>
+<body>
+    <div class="container">
+        <img src="assets/images/profil.jpg" alt="">
+        <ul class="daftar-data">
+            <li class="data-item">
+                <p class="title">Nama Lengkap</p>
+                <p class="item">Alif Fadhil Muhamad</p>
+            </li>
+            <li class="data-item">
+                <p class="title">Tempat, Tanggal lahit</p>
+                <p class="item">Kendari, 08 Mei 2002</p>
+            </li>
+            <li class="data-item">
+                <p class="title">Jenis Kelamin</p>
+                <p class="item">Laki-laki</p>
+            </li>
+            <li class="data-item">
+                <p class="title">Alamat lengkap</p>
+                <p class="item">Jl. Laode Abdul Kudus No.14</p>
+            </li>
+            <li class="data-item">
+                <p class="title">Riwayat Pendidikan</p>
+                <p class="item">SDN 8 Katobu</p>
+                <p class="item">MTs Pesantren Ummushabri Kendari</p>
+                <p class="item">SMAN 1 Raha</p>
+            </li>
+            <li class="data-item">
+                <p class="title">Hobi</p>
+                <p class="item">Musik</p>
+                <p class="item">Olahraga</p>
+                <p class="item">Gaming</p>
+            </li>
+        </ul>
+    </div>
+    
+</body>
+</html>
